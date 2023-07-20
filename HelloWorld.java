649338b19937dd4d22f84a8ce5bcc2a0c60c6ee2
mohan <mohanmac2022@gmail.com>
2023-07-20 22:56:13 +0530

    Changed value of a

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@ class HelloWorld
 {
     public static void main(String [] args)
     {
+        int num = 23;
         //System.out.println("Starts Here");
         System.out.println("Hello World...");
     }
