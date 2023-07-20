04a39d92f6f1bcd4da82fb3e77e7214e28733a8f
mohanbrock <140093098+mohanbrock@users.noreply.github.com>
2023-07-20 22:53:21 +0530

    Added num in the script

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@ class HelloWorld
 {
     public static void main(String [] args)
     {
+        int num =10;
         //System.out.println("Starts Here");
         System.out.println("Hello World...");
     }
