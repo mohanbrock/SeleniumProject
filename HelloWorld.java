b127eca2fec788bfba01c1109a0b123d5b758714
mohan <mohanmac2022@gmail.com>
2023-07-20 22:49:34 +0530

    Commented line 4

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ class HelloWorld
 {
     public static void main(String [] args)
     {
-        System.out.println("Starts Here");
+        //System.out.println("Starts Here");
         System.out.println("Hello World...");
     }
 }
