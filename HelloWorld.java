a29e0c39a2291c0fe29bd82ff30919287fafe4f5
mohanbrock <140093098+mohanbrock@users.noreply.github.com>
2023-07-20 23:02:17 +0530

    Removed value of num

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ class HelloWorld
     public static void main(String [] args)
     {
 
-        int num = 23;
+        int num;
         //System.out.println("Starts Here");
         System.out.println("Hello World...");
     }
